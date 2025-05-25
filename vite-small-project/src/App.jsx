39c05584f6588594171168a48259b01ab811b90c
saymon <slrivas24@gmail.com>
2025-05-25 19:43:31 -0400

    Created the first version of a landing page

--- vite-small-project/src/App.jsx
+++ vite-small-project/src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import NavigationBar from './components/NavigationBar'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1></h1>
+      <div className="App">
+
+        <NavigationBar />
+
+        <header className="App-header">
+          <h1>Galactic Contacts</h1>
+        </header>
+        <div className='App-content'>
+          <p>Welcome to Galactic Contacts! This is a simple React application that allows you to manage your contacts.</p>
+          <p>To get started, please log in or register.</p>
+          <p>Once logged in, you can add, edit, and delete contacts... At light speed.</p>
+          <p>Enjoy your journey building your galaxy of contacts!</p>
+        </div>
+      </div>
     </>
   )
 }
---
+++ vite-small-project/src/components/NavigationBar.jsx
@@ -0,0 +1,19 @@
+function NavigationBar() {
+  return (
+    <nav className="navbar">
+      <div>
+        <ul className="flex space-x-4">
+          <li><a href="/">Home</a></li>
+          <li><a href="/login">Login</a></li>
+          <li><a href="https://github.com/Dylankuneman04/SmallProject_COP4331C_22">Repo</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+// Saymon:
+// If logged in, show the user's name and a logout button
+// Possibly make a separate navbar component for logged in users?
+
+export default NavigationBar;
